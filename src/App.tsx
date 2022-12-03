95fdd1b2e70c6057066fdc2d54392d1ca8336e65
Matthew Wright <1815200+matthewguywright@users.noreply.github.com>
2022-12-02 20:37:52 -0600

    form started

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Container, Grid, Typography, Box, Button, Modal } from '@mui/material';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Container,
+  Grid,
+  Typography,
+  Box,
+  Button,
+  Modal,
+  InputLabel,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import axios from "axios";
 
 function App() {
   const [breedData, setBreedData] = useState([]);
+  const [breed, setBreed] = useState("");
+  const [subBreed, setSubBreed] = useState("");
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    bgcolor: "background.paper",
+    border: "2px solid #000",
     boxShadow: 24,
     pt: 2,
     px: 4,
     pb: 3,
   };
 
+  const logData = () => {
+    console.log(breed);
+    console.log(subBreed);
+  };
+
+  const handleBreedChange = (event: SelectChangeEvent) => {
+    setBreed(event.target.value);
+    logData();
+  };
+
+  const handleSubBreedChange = (event: SelectChangeEvent) => {
+    setSubBreed(event.target.value);
+    logData();
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(breed);
+    console.log(subBreed);
+    handleOpen();
+  };
+
   useEffect(() => {
     const cancelToken = axios.CancelToken.source();
 
     async function fetchBreeds() {
-      await axios.get('https://dog.ceo/api/breeds/list/all', { cancelToken: cancelToken.token })
+      await axios
+        .get("https://dog.ceo/api/breeds/list/all", {
+          cancelToken: cancelToken.token,
+        })
         .then((res) => {
           const breedArray = [];
           for (const breed in res.data.message) {
-            breedArray.push({ subBreeds: res.data.message[breed], breed });
+            breedArray.push({ sub: res.data.message[breed], name: breed });
           }
           setBreedData(res.data);
           console.log(res.data.message);
@@ -39,11 +79,11 @@ function App() {
         })
         .catch((err) => {
           if (axios.isCancel(err)) {
-            console.log('cancelled call');
+            console.log("cancelled call");
           } else {
             // TODO: message
           }
-        })
+        });
     }
     fetchBreeds();
   }, []);
@@ -52,12 +92,54 @@ function App() {
     <Container>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <h1>Breeds</h1>
+          <form id="dogForm" noValidate onSubmit={handleSubmit}>
+            <Grid item xs={12}>
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="breed-label">Breed</InputLabel>
+                <Select
+                  labelId="breed-label"
+                  id="breed"
+                  value={breed}
+                  label="Breed"
+                  onChange={handleBreedChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+                <FormHelperText>Choose a breed</FormHelperText>
+              </FormControl>
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="sub-breed-label">Sub-Breed</InputLabel>
+                <Select
+                  labelId="sub-breed-label"
+                  id="subBreed"
+                  value={subBreed}
+                  label="Sub-Breed"
+                  onChange={handleSubBreedChange}
+                  disabled
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+                <FormHelperText>Choose a sub-breed</FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="outlined">
+                submit
+              </Button>
+            </Grid>
+          </form>
         </Grid>
       </Grid>
-
-      <Button onClick={handleOpen}>Open modal</Button>
-      
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
